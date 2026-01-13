b278dd86a713fbfd752dbf6ba693a3e29f914cb5
Camilla Alves <arcamilla42@gmail.com>
2026-01-13 11:55:24 -0300

    refactor: rename HairStyles to HairType and update Hair class initialization

--- body_parts/hair.py
+++ body_parts/hair.py
@@ -1,14 +1,26 @@
 from enum import Enum
-
 from body_parts.base_body_parts import BaseBodyPart
 
 
-class HairStyles(Enum):
+class HairType(Enum):
+    NONE = "none"
     FUZZ = "fuzz"
-    #implement other styles as needed
+    # implement other styles as needed
+
 
 class Hair(BaseBodyPart):
-    def __init__(self, style: HairStyles) -> None:
+    def __init__(self) -> None:
         super().__init__("Hair")
-        self.style: HairStyles = style
+        self._type: HairType = HairType.NONE
         self.is_styled = False
+
+    @property
+    def type(self) -> HairType:
+        return self._type
+
+    @type.setter
+    def type(self, value: HairType) -> None:
+        self._type = value
+
+    def style_hair(self) -> None:
+        self.is_styled = True
